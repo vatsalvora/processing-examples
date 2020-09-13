e71d1c30b8474d44d000f36b3cf7c8aaa3273601
Vatsal Vora <vatsalajvora@gmail.com>
2020-09-13 19:49:47 -0400

    Arrays and For Loop

--- public/sketch.js
+++ public/sketch.js
@@ -1,14 +1,18 @@
-
+let list = ["Nand","Yashoda","Balram","Krishna"];
+let colors = ["Orange","Purple","Green","Blue"];
 function setup() {
   createCanvas(700, 700);
-  
 }
 
 function draw() {
-	let speak = "Jai Shree Krishna!"
-	console.log(speak)
-	textSize(32);
-	text(speak,40,50)
-	fill(255)
+	background(255);
+	let space = 0;
+	textSize(50);
+	for (x in list){
+		let c = color(colors[x]);
+		fill(c);
+		text(list[x],40,50+space);
+		space += 50;
+	}
 }
 
