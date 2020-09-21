8471e354cfb14e51e98d4843e9f42c6e49443297
Vatsal Vora <vatsalajvora@gmail.com>
2020-09-21 12:12:28 -0400

    Adding Music and Image

--- public/sketch.js
+++ public/sketch.js
@@ -1,11 +1,31 @@
 let list = ["Nand","Yashoda","Balram","Krishna"];
 let colors = ["Orange","Purple","Green","Blue"];
+let gameInfo = {"image":"assets/ground.jpg",
+"music":"assets/music.wav"};
+let img;
+let song;
+let SPACEKEY = 32;
+
+function preload(){
+	img = loadImage(gameInfo["image"]);
+	song = loadSound(gameInfo["music"]);
+}
+
 function setup() {
-  createCanvas(700, 700);
+  	createCanvas(700, 700);
+  	image(img, 0, 0);
+}
+
+function keyPressed(){
+	if(keyCode == SPACEKEY && !song.isLooping()){
+		song.loop();
+	}
+	else if(keyCode == SPACEKEY && song.isLooping()){
+		song.stop();
+	}
 }
 
 function draw() {
-	background(255);
 	let space = 0;
 	textSize(50);
 	for (x in list){
